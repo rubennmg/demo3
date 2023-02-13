d5c16ae6f0cd4cec7ba52963b4ceb107fb1e329a
rubennmg <rubennmg@github.com>
2023-02-13 13:16:34 +0100

    cambios desde la rama2

--- src/demo3/Demo.java
+++ src/demo3/Demo.java
@@ -4,5 +4,7 @@ public class Demo {
 	public static void main(String args[]) {
 		/* Rama master */
 		System.out.println("Primera carga");
+		/* Cambio desde la rama2 */
+		System.out.println("Cambio desde la rama2");
 	}
 }
